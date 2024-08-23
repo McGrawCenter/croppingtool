e6559610387f8fb20750a715584609cbf73357ee
benjohnsto <benj@princeton.edu>
2024-08-23 06:52:42 -0400

    v0.0.2

--- assets/js/pucropper.js
+++ assets/js/pucropper.js
@@ -1,8 +1,8 @@
 
-  var version = 2;
   var images = [];
   var label = "";
   var metadata = [];
+  var manifests = {};
   // this is so that we only empty the gallery
   // when someone adds a new manifest url
   // (a collection manifest contains multiple manifests
@@ -12,73 +12,80 @@
   // some manifests use 'max' for size rather than full (internet archive)
   // we need to detect it to generate our 'full' version
   var max_or_full = "full";
+
   
 
   /************************************
   * 
   *************************************/
-  function load(manifest_uri) {
-    
+  function load(url) {
 
-    jQuery("#gallery").empty();
+    //jQuery("#gallery").empty();
+    images = [];
+
+    // UCLA has a stupid 'ark:' in their urls that need to be encoded
+    url = url.replace(/ark:\/(.*?)\//,function(r,a){ return "ark%3A%2F"+a+"%2F"});
+
     
     // if this is an Internet Archive URL
     // convert it to a manifest
-    if(manifest_uri.indexOf("archive.org") > 0) {
-      manifest_uri = internetArchive2Manifest(manifest_uri);
-      
-    }    
-    console.log(manifest_uri);
-    
-    //console.log(manifest_uri.search(/\/([0-9]{1,3})\/(color|gray|bitonal|default)\.(png|jpg)/));
-    if(manifest_uri.search(/\/([0-9]{1,3})\/(color|gray|bitonal|default)\.(png|jpg)/) > 0) { 
-        parseSingleImage(manifest_uri)
-    }
+    if(url.indexOf("archive.org") > 0) { url = internetArchive2Manifest(urlurl);   }    
+
+
+    // if this is a IIIF image url, parse it
+    if(url.search(/\/([0-9]{1,3})\/(color|gray|bitonal|default)\.(png|jpg)/) > 0) { parseSingleImage(url);  }
     else {
-	    fetch(manifest_uri)
-		  .then(response => {
-		          if (!response.ok) {
-		              throw new Error(response.statusText);
-		          }
-		          return response.json();
-		  })
-		  .then(data => {
-		    // version 2
-		    if ("@type" in data) {
 
-		        if (data["@type"] == 'sc:Collection') {
-		            version = 2;
-		            parsev2Collection(data);
-		        }
-		        
-		        else if (data["@type"] == 'sc:Manifest') {
-		            version = 2;
-		            parsev2(data);
-		        }
-		        
-		        else {
-			   console.log( 'Manifest Format Error', 'The JSON for this Manifest doesnt look like a Manifest. It should have either a @type of sc:Manifest but has a type of: ' + data["@type"]);
-		        }
-		    } 
-		    // version 3
-		    else if ("type" in data) {
-		        if (data["type"] != 'Manifest') {
-		            console.log( 'Manifest Format Error', 'The JSON for this Manifest doesnt look like a Manifest. It should have either a type of Manifest but has a type of: ' + data["type"]);
-		        } else {
-		            version = 3;
-		            parsev3(data);
-		        }
-		    } 
-		    else {
-		        console.log( 'Manifest Format Error', 'The JSON for this Manifest doesnt look like a Manifest. It should have either a @type or type value of Manifest');
-		    }
+			    const vault = new IIIFVault.Vault();
+			    vault.loadManifest(url).then(async (manifest) => {
 
-		    
-		  })
-		  .catch(error => {
-		        console.log( 'Manifest retrieval error', 'I was unable to get the Manifest you supplied due to: ' + error);
-	      }); // end fetch
-	}  // end if/else
+			      var label = getFirstValue(manifest.label);
+
+			      
+			      manifest.metadata.forEach(function(meta){
+			        var meta_label = getFirstValue(meta.label);
+			        var meta_value = getFirstValue(meta.value);
+			        metadata.push({'label':meta_label, 'value': meta_value });
+			        //console.log(meta_label+": "+meta_value);
+			      });
+			      var o = {'label':label,'metadata':metadata}
+			      //console.log(o);
+			      manifests[url] = o;
+
+			      
+			      var items = vault.get(manifest.items);
+			      var type = manifest.type;
+			      
+			      switch(type){
+			      
+			        case 'Collection':
+			        items.forEach(function(item) {
+				    load(item.id);
+				});
+			        break;
+			        
+			        case 'Manifest':
+			          var items = vault.get(items);
+			          items.forEach(function(it){
+			            var canvas = it.id;
+			            var label = getFirstValue(it.label);
+			            var i = vault.get(it.items[0]);
+			            var j = vault.get(i.items[0]);
+			            var k = vault.get(j.body[0]);
+			            var service = k.service[0]['@id'];
+			            var x = {'service':service, 'manifest': url, 'canvas': canvas, 'label': label}
+			            images.push(x);
+			            
+			          });
+				  buildGallery(url);
+			        break;
+			      
+			      }
+			      
+			      });
+     
+          submitted = 1;
+     }  // end if/else
 	
   }
 
@@ -96,18 +103,9 @@
      }     
   }
 
-
-
   /************************************
   * 
   *************************************/
-  function parsev2Collection (manifest) {
-     for(const man of manifest.manifests) {
-       var service = man['@id'];
-       load(service);
-     }
-  }
-  
   function parseSingleImage(url) {
   
       var s = url.split("/").slice(0,-4);
@@ -123,245 +121,41 @@
       r.thumb = url;
       r.url = id;
       o.images.push(r);      
-      masterlist[id] = o;
+      //masterlist[id] = o;
       current_id = id;
       buildGallery(id);
   }
   
-  
-  /************************************
-  * Prase a version 2 manifest
-  *************************************/
-  function parsev2 (manifest) {
-
-      var id = getFirstValue(manifest['@id']);
-      
-      // initialize an object that will contain info
-      var o = {'label':'', 'metadata':[], images:[]}
-      
-      o.label = getFirstValue(manifest.label);
-      o.description = getFirstValue(manifest.description);
-      o.metadata = parseMetadata(manifest.metadata);    
-      
-      if(manifest.sequences) {
-        var sequences = manifest.sequences;
-        for (const sequence of sequences) {
-          if ('canvases' in sequence) {
-            for (const canvas of sequence.canvases) {
-              var thumb = getCanvasThumbnail(canvas, 150,150);
-              
-              if(thumb !== false) {
-                var label = canvas.label;
-                var url = canvas.images[0].resource.service["@id"];
-                var r = {}
-                r.label = label;
-                r.thumb = thumb;
-                r.url = url;
-                o.images.push(r);
-              }
-            }
-          }
-        }
-      }    
-      masterlist[id] = o;
-      current_id = id;
-      buildGallery(id);
-  }
 
 
-  /************************************
-  * Parse a version 3 manifest
-  *************************************/
-  function parsev3 (manifest) {
-  
-      var id = getFirstValue(manifest['id']);  
-      
-      
-      // initialize an object that will contain info
-      var o = {'label':'', 'metadata':[], images:[]}
-      
-      o.label = getFirstValue(manifest.label);
-      o.description = getFirstValue(manifest.description);
-      o.metadata = parseMetadata(manifest.metadata); 
-      
-      
-      // thumbnail
-      if(manifest.thumbnail) {
-	switch(typeof manifest.thumbnail) {
-	  case 'object':
-	    var thumbnail = manifest.thumbnail.id;
-	    break;
-	  case 'array':
-	    var thumbnail = manifest.thumbnail['id'];
-	    break;
-	  default: //string
-	    var thumbnail = manifest.thumbnail;
-	} 
-      }
-      else {
-        var thumbnail = manifest.items[0].items[0].items[0].body.service[0]['@id']+"/full/!150,150/0/default.jpg";
-      }
-     
-    
-      
-      if(manifest.items) {
-        var items = manifest.items;
-        
-        // check if manifest uses full or max for the size in image urls
-        // checking the id of the first 'item'
-        if(items[0].items[0].items[0].body.id.includes("/max/")) { max_or_full = "max"; }
-        
-        for (const item of items) {
 
-	      // label
-	      if(item.label) {
-		switch(typeof item.label) {
-		  case 'object':
-		    // get the first value
-		    var label = Object.values(item.label)[0][0];
-		    break;
-		  case 'array':
-		    var label = item.label[0];
-		    break;
-		  default: //string
-		    var label = item.label;
-		} 
-	      }
-	      else {
-		var label = "";
-	      }	        
-	    // end label --------------------------
-	    
-	    // service
-            var service = item.items[0].items[0].body.service;
-
-            if(typeof service === 'array') {
-              //console.log('array');
-              //console.log(service);
-            }
-            else if(typeof service === 'object') {
-              //console.log('object');
-              if(service[0]) {
-		 // sometimes v3 service ids have an @ sign, sometimes not 
-		 if(service[0]['id'] == null) { service = service[0]['@id'] }
-		 else { service = service[0]['id'] }
-                //service = service[0]['id'];
-              }
-              else {
-                service = service.id;
-              }
-            } 
-	     // end service ------------------------------
-	     
-	     
-	     
-	      // thumb
-	     if(item.thumbnail) {
-		switch(typeof item.thumbnail) {
-		  case 'object':
-		    //console.log('object');
-		    if(item.thumbnail[0]) { var thumb = item.thumbnail[0].id }
-		    else { var thumb = item.thumbnail.id; }
-		    break;
-		  case 'array':
-		    //console.log('array');
-		    var thumb = item.thumbnail[0];
-		    break;
-		  default: //string
-		    var thumb = item.thumbnail;
-		} 
-	     }
-	     else {
-		var thumb = service+"/full/!150,150/0/default.jpg";
-	     }              
-            // end thumb ------------------------------
-
-            //var url = item.items[0].items[0].body.service[0]['@id'];
-            
-            var url = service;
-            
-            var r = {}
-            r.label = label;
-            r.thumb = thumb;
-            r.label = label.replace(/"/g,'');
-            r.url = url;
-            o.images.push(r);
-        }
-      } 
-      masterlist[id] = o;
-      current_id = id; 
-      buildGallery(id);
-  }
-  
-  /************************************
-  * 
-  *************************************/
-  function getCanvasThumbnail (canvas, width, height) {
-  
-    if(canvas.thumbnail === undefined) {
-      if(typeof canvas.images !== 'undefined') { 
-         var thumbnail = canvas.images[0].resource.service['@id']+"/full/150,/0/default.jpg";
-      }
-      else {
-         return false;
-         //var thumbnail = "images/placeholder.jpg";
-      }
-    }
-    else {
-         if (typeof canvas.thumbnail === "string") { console.log("tres"); var thumbnail = canvas.thumbnail; }
-		 
-         else if (typeof canvas.thumbnail === "object") {
-             if(version==2) {
-	       //var thumbnail = canvas.thumbnail["@id"]+"/full/!"+width+", "+height+"/0/default.jpg";
-	       var thumbnail = canvas.thumbnail["@id"];
-	      }
-	      else {
-	       var thumbnail = canvas.thumbnail[0]["id"]+"/full/!"+width+", "+height+"/0/default.jpg";
-	      }
-	     
-         }  
-    }
-  
-   return thumbnail;   
-   }
-   
-   
    
 
   
   function buildGallery(id) {
-
-    var images = masterlist[id].images;
+  
+    //console.log(manifests[id]);
+  
     if(submitted == 1) {
        jQuery("#gallery").empty();
        submitted = 0;
     }
-    //console.log(masterlist);
-    var para = jQuery("<ul></ul>");
-
+    
+    var html = "<div>";
+    html += "<p class='gallery-manifest-label'>"+manifests[id].label+"</p>";
+    html += "<ul>";
     jQuery.each(images, function(i,v){ 
-      para.append("<li class='gallery-item' data-manifest='"+id+"' data-service='"+v.url+"' alt='image "+i+"'><img alt='"+v.label+"' src='"+v.thumb+"'/><div class='gallery-item-label'>"+v.label+"</div></li>");
-      //jQuery("#gallery ul").append();
-    });
-    jQuery("#gallery").append(para);
+      html += "<li class='gallery-item' data-manifest='"+v.manifest+"' data-canvas='"+v.canvas+"' data-service='"+v.service+"' alt='image "+i+"'><img alt='"+v.label+"' src='"+v.service+"/full/,200/0/default.jpg'/><div class='gallery-item-label'>"+v.label+"</div></li>";
+    }); 
+    html += "</ul>";
+    html += "</div>";
+    jQuery("#gallery").append(html);
     images = [];
   }
   
   
   
-  
-  
-  
-  
-  function getLabel(manifest) {
-    if(typeof manifest.label === "object") { return manifest.label[0]; }
-    else if(typeof manifest.label === "array") { return manifest.label[0]; }
-    else if(typeof manifest.label === "string") { console.log(manifest.label); }
-    else { return "";}
-  }
-  
-  
-  
+
   
   
   function parseMetadata(metadata) {
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,16 @@
   
 
   	var current_id = "";
+  	
   	// outputs is an object that holds the current output urls that will
   	// be displayed in the output window for whatever is currently selected
-  	
   	var outputs = {'manifest':'', 'service':'','detail':'','full':'','html':''};
+
   	
     	var overlay = false;
     	var selectionMode = false;	
     	var crop_url = "";
+    	var region = [];
   	var current_image = "";
   	var manifest_url = "";
   	var rotation = 0;
@@ -20,7 +22,7 @@
        * masterlist contains records of each manifest that is added to the tool
        * with title, descr, metadata, and images
        ***********************************/
-  	var masterlist = {};  	
+  	var masterlist = {};	
   	
   	/****************
   	* selections is a list of the crops
@@ -70,11 +72,14 @@
 	
 	jQuery(document).on("click",".gallery-item",function(e){
 	
+	  
 	  var manifest_url = jQuery(this).attr('data-manifest');
+	  var canvas = jQuery(this).attr('data-canvas');
 	  
 	  service = jQuery(this).attr('data-service');
 	  
-	  rotation = 0;
+	  //current = {'service':service, 'manifest': manifest_url, 'label': '', 'rotation': 0}
+
 	
 	  // highlight this gallery item
 	  jQuery(".gallery-item").removeClass('gallery-item-active');
@@ -90,17 +95,16 @@
 	  
 	  
 	  // i had orignally populate the output textarea with the full url when one clicked on the gallery item
-	  
-	  console.log(max_or_full);
 	  if(max_or_full == 'max') { var full_size = service+"/full/max/0/default.jpg"; }
 	  else { var full_size = service+"/full/full/0/default.jpg"; }
 
 	  outputs = {
 	    'manifest':manifest_url,
+	    'canvas': canvas,
 	    'service': service,
 	    'detail':full_size,
 	    'full':full_size,
-	    'html':"<img alt='' src='"+full_size+"' data-manifest='https://data.artmuseum.princeton.edu/iiif/objects/23888'/>"
+	    'html':"<img alt='' src='"+full_size+"' data-manifest='"+manifest_url+"'/>"
 	  }
 	  
 	  console.log(outputs);
@@ -184,22 +188,26 @@
 
 		var w = viewer.tileSources[0].width;
       
-		var p = [
+		region = [
 		    Math.floor(location.x * w),
 		    Math.floor(location.y * w),
 		    Math.floor(location.width * w),
 		    Math.floor(location.height * w)
 		]
-	      	
-	      	jQuery.each(p,function(i,v){ if(v < 0) { p[i] = 0; } });
+		
+	      	// if the box goes outside the boundaries of the image
+	      	jQuery.each(region,function(i,v){ if(v < 0) { region[i] = 0; } });
 	      		      
 		viewer.updateOverlay(drag.overlayElement, location);
 		
-		crop_url = outputs.service+"/"+p[0]+","+p[1]+","+p[2]+","+p[3]+"/"+overlayHeight+",/"+rotation+"/default.jpg";
+		crop_url = outputs.service+"/"+region[0]+","+region[1]+","+region[2]+","+region[3]+"/"+overlayHeight+",/"+rotation+"/default.jpg";
 		
-		// ben here
-	  if(max_or_full == 'max') { uncropped_url = outputs.service+"/"+p[0]+","+p[1]+","+p[2]+","+p[3]+"/max/"+rotation+"/default.jpg"; }
-	  else { uncropped_url = outputs.service+"/"+p[0]+","+p[1]+","+p[2]+","+p[3]+"/full/"+rotation+"/default.jpg" }		
+	        if(max_or_full == 'max') { 
+	          uncropped_url = outputs.service+"/"+region[0]+","+region[1]+","+region[2]+","+region[3]+"/max/"+rotation+"/default.jpg";
+	        }
+	        else { 
+	          uncropped_url = outputs.service+"/"+region[0]+","+region[1]+","+region[2]+","+region[3]+"/full/"+rotation+"/default.jpg";
+	        }		
 		
 			
 		
@@ -211,35 +219,26 @@
 		if(selectionMode==true) { 
 		
 		    manifest_url = jQuery("#url").val();
-		    console.log(manifest_url);
-		    console.log(masterlist);
-		    console.log(rotation);
-		    //var img_title = masterlist[manifest_url].label;
-		    var img_title = masterlist[current_id].label
+
 		    var img_html = "<img alt='thumbnail image' src='"+crop_url+"' data-manifest='"+manifest_url+"'/>";
-		    
+		    console.log(outputs);
 		    
 		    // add info to the selections array
 		    // creating an id would probably be good
 		    var selection_index = selections.push({"id":"", "manifest":manifest_url,"detail":crop_url,"html":img_html, "full":uncropped_url, "mode": "detail"})-1;
 		    
+		    //console.log(selections);
+		    
 		    // if any items in the tray are currently active, remove active class
 		    jQuery(".preview-item.active-item").removeClass('active-item'); 		    
 
 
-
-
-
-
-
-
 		    //construct html of thumbnail in bottom tray
 
-		    //var preview_item = "<div class='preview-item active-item' data-num='"+selections.length+"' data-selection='"+selection_index+"'>\
-		    var preview_item = "<div class='preview-item active-item' data-service='"+outputs.service+"' data-selection='"+selection_index+"'>\
+		    var preview_item = "<div class='preview-item active-item' data-service='"+outputs.service+"' data-canvas='"+outputs.canvas+"' data-manifest='"+manifest_url+"' data-selection='"+selection_index+"'>\
 		    <a href='#' class='selectcrop'>"+img_html+"</a>\
 		    <span class='preview-item-tools'>\
-		     <a href='#' class='preview-item-metadata' rel='"+manifest_url+"'><img src='assets/images/info-circle-white.svg' height='15'/></a>\
+		     <a href='#' class='preview-item-metadata'><img src='assets/images/info-circle-white.svg' height='15'/></a>\
 		     <a href='"+crop_url+"' class='preview-item-external' target='_blank'><img src='assets/images/external-white.svg' height='15'/></a>\
 		     <a href='#' class='preview-item-close'><img src='assets/images/x-white.svg' height='15'/></a></span></div>";
 
@@ -369,7 +368,8 @@
 	  jQuery(".preview-item").removeClass('active-item');
 	  jQuery(this).addClass("active-item");
 	    
-	  var manifest_url = jQuery(this).attr('rel');
+	  var manifest_url = jQuery(this).attr('data-manifest');
+
 	  var selection_index = jQuery(this).attr('data-selection');
 	  var service = jQuery(this).attr('data-service');
 	  
@@ -386,21 +386,19 @@
 	  
 	  //re-populate the url text field with the manifest url for this detail
 	  var previous_manifest_url = jQuery("#url").val();
-	  var selection_index = jQuery(this).attr('data-selection');
-	  var current_manifest_url = selections[selection_index].manifest;
-	  jQuery("#url").val(current_manifest_url);
+	  
+	  //var selection_index = jQuery(this).attr('data-selection');
+	  //var current_manifest_url = selections[selection_index].manifest;
+	  jQuery("#url").val(manifest_url);
 	  // if we are changing to a different manifest, reload the gallery of thumbs
-	  if(previous_manifest_url != current_manifest_url) {
-	      load(current_manifest_url);
+	  if(previous_manifest_url != manifest_url) {
+	      load(manifest_url);
 	  }
 	  
 	  //populate the output textarea with whatever mode is currently selected
 	  //var mode = jQuery("#output").attr('data-mode');
 	  updateOutputURLs();
 	  
-	  //jQuery("#output").val(selections[selection_index][mode]);
-	  
-	  //jQuery("#output").attr('data-current',selection_index);
 	});
 	
 
@@ -454,18 +452,34 @@
 	
 	jQuery(document).on("click", ".preview-item-metadata", function(e) {
 	
+	  var manifest = jQuery(this).parent().parent().attr('data-manifest');
+	  var canvas = jQuery(this).parent().parent().attr('data-canvas');
+	  
+	  console.log("Manifest: "+manifest);
+	  console.log("Canvas: "+canvas);
+	  console.log("Region: "+region.join(','));
+	  
+	  
 	   // update the urls that go in the 'copy' textarea
 	   updateOutputURLs();
-	
-	   var  o = masterlist[current_id];
+
+	   var  o = manifests[manifest];
 
 	   var html = "";
-	   html += "<p><label for='title'>Title</label><span id='title'>"+o.label+"</span></p>";
-	   html += "<p><label for='descr'>Description</label><span id='descr'>"+o.description+"</span></p>";
+	   html += "<p><label for='title'>Title:</label> <span id='title'>"+o.label+"</span></p>";
+	   html += "<p><label for='descr'>Description:</label> <span id='descr'>"+o.description+"</span></p>";
 	   
 	   jQuery.each(o.metadata, function(i,v) {
-	     html += "<p><label for='"+v.label+"'>"+v.label+"</label><span id='"+v.label+"'>"+v.value+"</span></p>";
+	     html += "<p><label for='"+v.label+"'>"+v.label+":</label> <span id='"+v.label+"'>"+v.value+"</span></p>";
 	   });
+	   html += "<p><a href='"+manifest+"' target='_blank'><img src='assets/images/iiif.svg' class='icon'/></a></p>";
+	   
+	   html += "<p><strong>Manifest</strong>: "+manifest+"</p>";
+	   html += "<p><strong>Canvas</strong>: "+canvas+"</p>";
+	   html += "<p><strong>Region</strong>: "+region+"</p>"
+	   
+	   
+	   
 	   jQuery('#modal_content').html(html);
 	   jQuery('#modal').modal();
 
